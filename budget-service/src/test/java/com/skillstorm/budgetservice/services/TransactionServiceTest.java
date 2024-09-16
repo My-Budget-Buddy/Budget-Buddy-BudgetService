3f6e6b47d650419fa45b26b0a0f81f5592301317
Bruna B. Vicente <bvicente.bruna@gmail.com>
2024-09-16 13:17:53 -0400

    Added more code to the TransactionServiceTest, but the code is still not working

--- budget-service/src/test/java/com/skillstorm/budgetservice/services/TransactionServiceTest.java
+++ budget-service/src/test/java/com/skillstorm/budgetservice/services/TransactionServiceTest.java
@@ -13,39 +13,65 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestClient;
 
 import com.skillstorm.budgetservice.dto.TransactionDTO;
 
+@RestClientTest(TranscationService.class)
 public class TransactionServiceTest {
 
     @Mock
-    private static LoadBalancerClient loadBalancerClient;
+    private LoadBalancerClient loadBalancerClient;
+
+    // @Mock
+    // private RestClient restClient;
+    @Autowired
+    private MockRestServiceServer server;
 
     @InjectMocks
     private TranscationService transactionSvc;
+    private AutoCloseable closeable;
 
-    // @Test
-    // public void getTransactionsExcludingIncomeTest() {
-    //     int userId = 1;
-    //     ServiceInstance serviceInstance = new TestServiceInstance();
-    //     when(loadBalancerClient.choose(any(String.class))).thenReturn(serviceInstance);
+    @BeforeEach
+    public void setup() {
+        closeable = org.mockito.MockitoAnnotations.openMocks(this);
+        // restClient = RestClient.builder().build();
+    }
 
-    //     // verify(loadBalancerClient).choose(any(String.class)).equals(serviceInstance);
-    //     // TransactionDTO transaction = new TransactionDTO();
+    @AfterEach
+    public void tearDown() throws Exception {
+        closeable.close();
+    }
 
-    //     // Optional<TransactionDTO> accountOptional = Optional.of(transaction);
-    //     // when(accountRepository.findById(any(int.class))).thenReturn(accountOptional);
+    @Test
+    public void getTransactionsExcludingIncomeTest() {
+        int userId = 1;
+        ServiceInstance serviceInstance = new TestServiceInstance();
+        when(loadBalancerClient.choose(any(String.class))).thenReturn(serviceInstance);
 
-    //     // List<TransactionDTO> response = transactionSvc.getTransactionsExcludingIncome(userId);
-    //     // List<TransactionDTO> expectedAccounts = new ArrayList<>();
+        // this.server.expect(requestTo("/transactionsPrivate/budget/" + userId))
+        //   .andRespond(withSuccess(detailsString, MediaType.APPLICATION_JSON));
 
-    //     // assertEquals(expectedAccounts, response);
-    // }
+        List<TransactionDTO> expected = new ArrayList<>();
+        // when(restClient.get().uri(any(String.class)).retrieve().body(new ParameterizedTypeReference<List<TransactionDTO>> () {})).thenReturn(expected);
+
+        List<TransactionDTO> response = transactionSvc.getTransactionsExcludingIncome(userId);
+        
+
+        assertEquals(expected, response);
+    }
 
     private class TestServiceInstance implements ServiceInstance {
 
